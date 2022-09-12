4f00b5fbc27d7c414d9e780f51be45d652c32fbd
oneqrhee <blaackpurin@gmail.com>
2022-09-13 00:27:36 +0900

    [기능개발] 오류수정

--- src/main/java/com/example/itmonster/domain/Channel.java
+++ src/main/java/com/example/itmonster/domain/Channel.java
@@ -4,11 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Builder
@@ -22,6 +25,8 @@ public class Channel {
     private Long id;
 
     @OneToOne
+    @JoinColumn
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Quest quest;
 
     private String channelName;
